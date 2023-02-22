eb78940cc603f5026f4a460153fc44191e02a5b3
王強 <oukyou@192.168.0.101>
2023-02-22 16:59:13 +0800

    动画练习

--- lib/widget/animation/animation_one_screen.dart
+++ lib/widget/animation/animation_one_screen.dart
@@ -17,6 +17,8 @@ class AnimationOneScreen extends StatefulWidget {
 }
 
 class _AnimationOneScreenState extends State<AnimationOneScreen> {
+  double width = 200;
+
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
@@ -28,10 +30,99 @@ class _AnimationOneScreenState extends State<AnimationOneScreen> {
         ),
         body: Container(
           alignment: Alignment.center,
-          child: const AnimationOne(
-            width: 200,
-            renderPercentage: 0.9,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              SmartAnimation(
+                key: UniqueKey(),
+                size: Size(width, 40),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // AnimationOne这种方式不推荐，持有状态值太多。使用TweenAnimationBuilder最合适
+              AnimationOne(
+                key: UniqueKey(),
+                width: width,
+                renderPercentage: 0.9,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20), //角の丸み
+                  side: const BorderSide(color: Colors.red), //枠線の設定
+                ),
+                color: Colors.blue,
+                child: const Text(
+                  'restart animation',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  setState(() {
+                    width = 200;
+                  });
+                },
+              )
+            ],
           ),
         ));
+  }
+}
+
+class SmartAnimation extends StatelessWidget {
+  final Size size;
+
+  const SmartAnimation({Key? key, required this.size}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+        tween: Tween<double>(begin: 0, end: size.width - 50),
+        duration: const Duration(seconds: 1),
+        curve: Curves.easeInOutCubic,
+        builder: (context, anim, child) {
+          return SizedBox(
+            width: size.width,
+            child: Stack(
+              children: [
+                Container(
+                  width: size.width,
+                  height: size.height,
+                  decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        begin: FractionalOffset.centerLeft,
+                        end: FractionalOffset.centerRight,
+                        colors: [Colors.yellow, Colors.black],
+                      ),
+                      color: Colors.red,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey[400]!,
+                            blurRadius: 2,
+                            offset: Offset(0, 2))
+                      ],
+                      borderRadius: BorderRadius.circular(15)),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    width: size.width - anim,
+                    height: size.height,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: anim == 0
+                            ? BorderRadius.circular(15)
+                            : const BorderRadius.horizontal(
+                                right: Radius.circular(15))),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
