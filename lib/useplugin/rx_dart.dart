db2b87257651f15ac6f510c9c0f6ab08506c7ded
王強 <oukyou@192.168.0.102>
2023-07-12 09:08:05 +0800

    rxdart

---
+++ lib/useplugin/rx_dart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RxDartScreen extends StatefulWidget {
+  const RxDartScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RxDartScreen> createState() => _RxDartScreenState();
+}
+
+class _RxDartScreenState extends State<RxDartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
