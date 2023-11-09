9862bafa65d6187b5ed362fb494699d4f2bc5137
王強 <oukyou@192.168.0.102>
2023-11-09 09:53:05 +0800

    ripple效果。

--- lib/widget/button/examination_upload_button.dart
+++ lib/widget/button/examination_upload_button.dart
@@ -10,35 +10,37 @@ class ExaminationUploadButton extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     return Material(
       color: Colors.white,
-      borderRadius: BorderRadius.circular(5.0),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(5.0),
-        child: Container(
-            alignment: Alignment.center,
-            width: 300,
-            height: 54,
-            decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Color(0xFF63C54B), Color(0xFF3FB592)],
-                ),
-                borderRadius: BorderRadius.circular(50.0)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Assets.examinationCancer.image(width: 25, height: 25),
-                const SizedBox(width: 6),
-                Text(
-                  "健康診断結果を登録する",
-                  style: textTheme.titleMedium?.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                )
-              ],
-            )),
-        onTap: () {
-          AppDebug.log("-------------");
-        },
+      borderRadius: BorderRadius.circular(50.0),
+      child: Ink(
+        decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFF63C54B), Color(0xFF3FB592)],
+            ),
+            borderRadius: BorderRadius.circular(50.0)),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(50.0),
+          child: Container(
+              alignment: Alignment.center,
+              width: 300,
+              height: 54,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Assets.examinationCancer.image(width: 25, height: 25),
+                  const SizedBox(width: 6),
+                  Text(
+                    "健康診断結果を登録する",
+                    style: textTheme.titleMedium?.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  )
+                ],
+              )),
+          onTap: () {
+            AppDebug.log("-------------");
+          },
+        ),
       ),
     );
   }
